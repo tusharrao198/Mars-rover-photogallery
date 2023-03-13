26ffd1ac72d6e0c0fe0a9528998e2abbe8fe0df5
Tushar Rao <tushar.rao198@gmail.com>
2023-03-13 16:08:14 +0530

    working springboot application

---
+++ src/test/java/com/sic/marsroverapi/MarsRoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sic.marsroverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
