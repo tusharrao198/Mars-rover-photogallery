f352194faaa5f57b9675c1b9daa71aa4063ba2ee
Tushar Rao <tushar.rao198@gmail.com>
2023-03-13 22:11:08 +0530

    added thymeleaf support

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Mars-rover-photogallery
 Java Springboot Application
+
+
+- To resolve dependencies : `ctrl + Shift + O`
---
+++ src/main/java/com/sic/marsroverapi/HomeController.java
@@ -0,0 +1,13 @@
+package com.sic.marsroverapi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getHomeView(Model model) {
+        model.addAttribute("message", "Welcome to MARS!");
+        return "home";
+    }
+}
